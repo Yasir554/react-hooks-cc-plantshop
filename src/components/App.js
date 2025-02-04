87624a758ef12e84cdfa2a0b9576ce25203d3a14
yasir <yasirabass554@gmail.com>
2025-02-04 22:44:27 +0300

    added soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import React, { useEffect, useState } from 'react';
 
-function App() {
+function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(response => response.json())
+      .then(data => setPlants(data));
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-      <PlantPage />
-    </div>
+    <ul className="cards">
+      {plants.map(plant => (
+        <li key={plant.id} className="card" data-testid="plant-item">
+          <img alt={plant.name} src={plant.image} />
+          <h4>{plant.name}</h4>
+          <p>Price: {plant.price}</p>
+          <button className="primary">In Stock</button>
+          <button>Delete</button>
+          <form>
+            <input placeholder="New price..." step="0.01" type="number" defaultValue={plant.price} />
+            <button type="submit">Update Price</button>
+          </form>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+export default PlantPage;
